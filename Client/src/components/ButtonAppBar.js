50ead3696c5c29bb4b47b98adea725481747f26d
cvanauken <coopervanauken@gmail.com>
2022-10-10 07:53:42 -0600

    added linked buttons to MainLayout, syntax error in Footer

--- Client/src/components/ButtonAppBar.js
+++ Client/src/components/ButtonAppBar.js
@@ -6,6 +6,7 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
 
 export default function ButtonAppBar() {
   return (
@@ -24,7 +25,18 @@ export default function ButtonAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             News
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">
+            <Link to="/Home">Home</Link>
+          </Button>
+          <Button color="inherit">
+            <Link to="/OnOrder">On Order</Link>
+          </Button>
+          <Button color="inherit">
+            <Link to="/ToOrder">To Be Ordered</Link>
+          </Button>
+          <Button color="inherit">
+            <Link to="/OutOfStock">86 List</Link>
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
