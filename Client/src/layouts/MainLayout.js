7b7737f2f360d6d4717e98192a00490075f31a41
AugustusXI <103222292+AugustusXI@users.noreply.github.com>
2022-10-10 16:38:10 -0600

    fixed mongo uri error

--- Client/src/layouts/MainLayout.js
+++ Client/src/layouts/MainLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ButtonAppBar from "../components/ButtonAppBar";
 import Footer from "../components/Footer";
-import Home from "../pages/Home";
+import Home from "../pages/home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const MainLayout = () => {
   return (
