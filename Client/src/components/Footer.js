d1d069e22140b9f7f9bfbe3d7f506eee7db2c630
Gatsby <87589924+BrandonOkman@users.noreply.github.com>
2022-10-10 13:17:30 -0600

    fixed incomsistent naming

--- Client/src/components/Footer.js
+++ Client/src/components/Footer.js
@@ -1,12 +1,12 @@
-import {Grid} from "@mui/material"
+import Grid from "@mui/material/Grid"
 import React from 'react';
 
 const Footer = () => {
-    return{
-        <Grid ;display='flex' ;width='100%' ;style={{position 'absolute', bottom: 0}}>
+    return(
+        <Grid >
             Footer
         </Grid>
-    }
+    )
 }
 
 export default Footer
