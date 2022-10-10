9886bae8edf4b5c70e65cdb8bcda9a88eb6768f7
Gatsby <87589924+BrandonOkman@users.noreply.github.com>
2022-10-10 12:37:24 -0600

    installed in frontend

---
+++ Client/src/layouts/MainLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ButtonAppBar from '../components/ButtonAppBar';
+import Footer from '../components/Footer';
+const MainLayout = () => {
+    return (
+    <>
+    <ButtonAppBar />
+    <Footer />
+    </>
+    );
+};
+export default MainLayout;
