1e983a30c41e722268b306b3997dfaacc7837cb2
AugustusXI <103222292+AugustusXI@users.noreply.github.com>
2022-10-11 02:18:29 -0600

    Delete Client directory

--- Client/src/pages/OnOrder.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const onOrder = () => {
-  return <div>On Order</div>;
-};
-
-export default onOrder;
--- Client/src/pages/OutOfStock.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const outOfStock = () => {
-  return <div>Out of Stock</div>;
-};
-
-export default outOfStock;
