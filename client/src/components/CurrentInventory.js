e63aa0d900b71031a9abe7c63d3d914ab5d928ff
AugustusXI <103222292+AugustusXI@users.noreply.github.com>
2022-10-11 04:14:47 -0600

    built CurrentInventory component

---
+++ client/src/components/CurrentInventory.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const CurrentInventory = ({ inventory = [] }) => {
+  if (!inventory.length) {
+    return <h3>We're Completely Out of Stock</h3>;
+  }
+
+  return (
+    <>
+      <h3
+        className="p-5 display-inline-block"
+        style={{ borderBottom: "1px dotted #1a1a1a" }}
+      >
+        Inventory
+      </h3>
+      <div className="flex-row my-4">
+        {inventory &&
+          inventory.map((food) => (
+            <div key={food._id} className="col-12 mb-3 pb-3">
+              <div className="p-3 bg-dark text-light">
+                <h5 className="card-header">
+                  {food.name}
+                  <span style={{ fontSize: "0.825rem" }}>
+                    on {food.inStock}
+                  </span>
+                </h5>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default CurrentInventory;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { IN_STOCK } from "../utils/queries";
+import CurrentInventory from "../components/CurrentInventory";
 const Home = () => {
   const { loading, data } = useQuery(IN_STOCK);
 
-  return <div>home</div>;
+  return (
+    <main>
+      <div className="flex-row justify-center">
+        <div
+          className="col-12 col-md-10 mb-3 p-3"
+          style={{ border: "1px dotted #1a1a1a" }}
+        >
+          <CurrentInventory />
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
