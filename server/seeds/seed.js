6d13c684ec1c4ca7699532f4f629266230ad4b70
AugustusXI <103222292+AugustusXI@users.noreply.github.com>
2022-10-11 03:37:23 -0600

    Definitely didnt just delete everything

---
+++ server/seeds/seed.js
@@ -0,0 +1,13 @@
+const db = require("../config/connection");
+const { Food } = require("../models");
+
+const FoodData = require("./FoodData.json");
+
+db.once("open", async () => {
+  await Food.deleteMany({});
+
+  const foodItems = await Food.insertMany(FoodData);
+
+  console.log("Menu seeded!");
+  process.exit(0);
+});
