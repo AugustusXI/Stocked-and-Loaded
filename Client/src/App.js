7a43d87d34beb195252e69faa6daf8ab44a836b4
AugustusXI <103222292+AugustusXI@users.noreply.github.com>
2022-10-10 18:48:46 -0600

    Page renders

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,24 @@
-import MainLayout from "./layouts/MainLayout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import ButtonAppBar from "./components/ButtonAppBar";
+
 function App() {
-  return <MainLayout />;
+  return (
+    // <Home />
+    <Router>
+      <>
+        <ButtonAppBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<onOrder />} /> */}
+          {/* <Route path="/" element={<outOfStock />} /> */}
+          {/* <Route path="/" element={<toOrder />} /> */}
+        </Routes>
+        <Footer />
+      </>
+    </Router>
+  );
 }
 
 export default App;
