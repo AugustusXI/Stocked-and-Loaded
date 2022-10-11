4d3f80a4844d970e5af7d2c4158c6b5364f9c312
AugustusXI <103222292+AugustusXI@users.noreply.github.com>
2022-10-11 02:44:54 -0600

    File restructure

--- Server/utils/queries.js
+++ Server/utils/queries.js
@@ -1,7 +1,18 @@
 import { gql } from "@apollo/client";
-new GraphQLObjectType({
-  name: "Query",
-  fields: {
-    _dummy: { type: graphql.GraphQLString },
-  },
-});
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+    }
+  }
+`;
+export const IN_STOCK = gql`
+  query Query {
+    inStock {
+      name
+      inStock
+    }
+  }
+`;
