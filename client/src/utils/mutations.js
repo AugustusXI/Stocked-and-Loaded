b5630c0430d093893df2addcc0bce7b2fa64ceec
AugustusXI <103222292+AugustusXI@users.noreply.github.com>
2022-10-12 14:35:04 -0600

    khjb

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,13 +12,13 @@ export const LOGIN_USER = gql`
   }
 `;
 
-export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
+export const UPDATE = gql`
+  mutation update($name: String!, $inStock: Boolean!, $onOrder: Boolean!) {
+    update(name: $name, inStock: $inStock, onOrder: $onOrder) {
+      Food {
+        name
+        inStock
+        onOrder
       }
     }
   }
